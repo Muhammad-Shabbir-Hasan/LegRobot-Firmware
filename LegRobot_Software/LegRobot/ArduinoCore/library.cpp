68c765d94228e4185ff45580f4bbe935d75debf0
Muhammad-Shabbir-Hasan <m.shabbir.hasan@hotmail.com>
2023-05-23 21:44:05 +0300

    Empty Project

---
+++ LegRobot_Software/LegRobot/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 5/23/2023 9:35:33 PM
+ * Author : 35846
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
